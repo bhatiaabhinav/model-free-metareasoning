6cf7aaf1fe13662cbd20e5462a504d4f657545cd
Justin Svegliato <justin.svegliato@gmail.com>
2020-05-21 11:25:50 -0400

    Cleaned up the utils file

--- MFMR/utils.py
+++ MFMR/utils.py
@@ -15,11 +15,6 @@ def load(filename):
         return json.load(file)
 
 
-def save_policy(policy, filename):
-    with open(filename, 'w') as file:
-        json.dump(policy, file, sort_keys=True, indent=4)
-
-
 def digitize(item, bins):
     for i, _ in enumerate(bins):
         if i + 1 < len(bins):
@@ -34,29 +29,11 @@ def get_dataset(problem_file, increment):
     dataset = []
     for instance in instances.values():
         entries = list(enumerate(instance["estimated_qualities"]))
-        dataset.append([(quality, round(time / increment)) for time, quality in entries[0:len(entries):increment]])
+        dataset.append([(quality, round(time / increment))
+                        for time, quality in entries[0:len(entries):increment]])
 
     return dataset
 
 
 def get_time_dependent_utility(quality, time, alpha, beta):
     return alpha * quality - math.exp(beta * time)
-
-
-def get_smoothed_values(data, window_size):
-    smoothed_values = []
-
-    for i in range(1, len(data)):
-        truncated_data = data[:i]
-        windowed_data = truncated_data[-window_size:]
-        smoothed_values.append(np.average(windowed_data))
-
-    return smoothed_values
-
-
-def get_results(errors, window_size, plot_window_size):
-    return {
-        "mean_error": np.average(errors[-window_size:]),
-        "standard_deviation_error": stats.sem(errors[-window_size:]),
-        "smoothed_errors": get_smoothed_values(errors, plot_window_size)
-    }
