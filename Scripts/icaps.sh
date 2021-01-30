873c8dcf0ccb1a04c6e9d81c5370b513bc310acb
Abhinav Bhatia <abhinav.bhatia.me@gmail.com>
2021-01-30 04:21:53 -0500

    cleanup

--- Scripts/icaps.sh
+++
@@ -1,5 +0,0 @@
-A2.0Astar-4puzzle-v0 Random 200000 --num_episodes_to_run=500 --algo_suffix=seed10_W1_tc10_sim_4puzzle_icaps --tags static W1 sim icaps 4puzzle --seed=10 --RPE_av_over=500 --no_render --no_monitor --overwrite
-A2.0Astar-4puzzle-v0 Random 200000 --num_episodes_to_run=500 --algo_suffix=seed10_random_tc10_sim_4puzzle_icaps --tags icaps random sim 4puzzle --seed=10 --RPE_av_over=500 --no_render --no_monitor --overwrite
-
-A2.0Astar-inv-4puzzle-v0 Random 200000 --num_episodes_to_run=500 --algo_suffix=seed10_W1_tc10_sim_4puzzle_icaps --tags static W1 sim icaps 4puzzle --seed=10 --RPE_av_over=500 --no_render --no_monitor --overwrite
-A2.0Astar-inv-4puzzle-v0 Random 200000 --num_episodes_to_run=500 --algo_suffix=seed10_random_tc10_sim_4puzzle_icaps --tags icaps random sim 4puzzle --seed=10 --RPE_av_over=500 --no_render --no_monitor --overwrite
--- Scripts/tsp.sh
+++
@@ -1,9 +0,0 @@
-python -m MFMR.deepRL A2.0Astar-20tsp-B0.3-v0 DQN 100000 --double_dqn --algo_suffix=D_W2_ep0.01 --tags W2 --framestack=20 --seed=0 --hiddens 128 64 --train_freq=1 --exp_buff_len=100000 --mb_size=32 --grad_clip=1 --target_q_freq=1 --target_q_polyak=0.999 --min_explore_steps=1000 --ep_anneal_steps=19000 --ep=0.01 --no_render --overwrite --no_gpu
-
-
-python -m MFMR.deepRL A2.0Astar-20tsp-B0.3-v0 DQN 100000 --double_dqn --algo_suffix=D_W2_ep0.01_evaluate --tags W2 evaluate --framestack=20 --seed=0 --hiddens 128 64 --train_freq=1 --exp_buff_len=100000 --mb_size=32 --grad_clip=1 --target_q_freq=1 --target_q_polyak=0.999 --min_explore_steps=1000 --ep_anneal_steps=19000 --ep=0.0 --no_render --overwrite --no_gpu --eval_mode --eval_run=DQN_D_W2_ep0.01 --monitor_video_freq=1
-
-
-python -m MFMR.deepRL A2.0Astar-20tsp-B0.3-v0 DQN 100000 --double_dqn --algo_suffix=D_dynW_ep0.01 --tags dynW --framestack=20 --seed=0 --hiddens 128 64 --train_freq=1 --exp_buff_len=100000 --mb_size=32 --grad_clip=1 --target_q_freq=1 --target_q_polyak=0.999 --min_explore_steps=1000 --ep_anneal_steps=19000 --ep=0.01 --no_render --overwrite --no_gpu
-
-python -m MFMR.deepRL A2.0Astar-20tsp-B0.3-v0 DQN 100000 --double_dqn --algo_suffix=D_dynW_ep0.01_evaluate --tags dynW evaluate --framestack=20 --seed=0 --hiddens 128 64 --train_freq=1 --exp_buff_len=100000 --mb_size=32 --grad_clip=1 --target_q_freq=1 --target_q_polyak=0.999 --min_explore_steps=1000 --ep_anneal_steps=19000 --ep=0.0 --no_render --overwrite --no_gpu --eval_mode --eval_run=DQN_D_dynW_ep0.01 --monitor_video_freq=1
